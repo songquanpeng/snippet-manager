b870b04e574ccc4659839a11102eb8f2667062c0
Song <quanpengsong@gmail.com>
2021-02-19 21:01:41 +0800

    :tada: it works now

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh()],
+  server: {
+    proxy: {},
+  },
+  devServer: {
+    proxy: 'http://localhost:3000',
+  },
+  build: {
+    outDir: 'build',
+  },
+});
