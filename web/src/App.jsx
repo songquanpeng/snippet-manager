196ff3747762891157b90fc6ce3892c9d6991f16
Song <quanpengsong@gmail.com>
2021-02-19 21:26:20 +0800

    :tada: add loading screen

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { useRoutes } from 'react-router-dom';
 
 // import { ThemeProvider } from '@material-ui/core';
@@ -6,6 +7,16 @@ import { mainTheme } from './theme';
 import routes from './routes';
 
 const App = () => {
+  useEffect(() => {
+    const e = document.getElementById('ipl-progress-indicator');
+    if (e) {
+      e.classList.add('available');
+      setTimeout(() => {
+        e.outerHTML = '';
+      }, 2000);
+    }
+  }, []);
+
   const routing = useRoutes(routes);
   return <>{routing}</>;
 };
