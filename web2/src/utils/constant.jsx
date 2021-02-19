213bb70fdea15a0780bab540e9d5185a43b6fcaa
Song <quanpengsong@gmail.com>
2021-02-19 20:44:55 +0800

    :fire: delete old web dir

--- web2/src/utils/constant.jsx
+++
@@ -1,7 +0,0 @@
-const constant = {
-  tagListWidth: 240,
-  snippetListWidth: 240,
-  topBarHeight: 64,
-};
-
-export default constant;
--- web2/src/views/NotFoundView.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const NotFoundView = () => {
-  return <div>404</div>;
-};
-
-export default NotFoundView;
