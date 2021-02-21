824b2707f5ce536b27b26d96e741901ae46201f1
Song <quanpengsong@gmail.com>
2021-02-21 19:12:01 +0800

    :construction: create a default user when user table is empty

--- model/database.go
+++ model/database.go
@@ -1,6 +1,8 @@
 package model
 
 import (
+	"errors"
+	"fmt"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 	"log"
@@ -16,5 +18,15 @@ func init() {
 	} else {
 		log.Fatal(err)
 	}
+	user := User{}
+	if err := db.First(&user).Error; errors.Is(err, gorm.ErrRecordNotFound) {
+		user.Username = "admin"
+		user.Password = "123456"
+		user.IsAdmin = true
+		if err := DB.Create(&user).Error; err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+
 	return
 }
